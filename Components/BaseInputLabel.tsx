358b6761f59705d5405d7bf0d087b48c4de1961d
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-02 16:27:19 +0100

    add BaseInputLabel & IconsOutlineInfo

--- Components/BaseInputLabel.tsx
+++ Components/BaseInputLabel.tsx
@@ -1,217 +1,79 @@
-import React, { ReactElement } from 'react';
-import {View, Text} from 'react-native';
-import { StyleSheet } from 'react-native';
-import theme from './theme';
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
 
 export interface BaseInputLabelProps {
-  labelPrimary: string,
-  labelRight: string,
-  labelSecondary: string,
-  color: 'Dark' | 'Grey' | 'Green' | 'Red' | 'White',
-  hasIconLeft?: boolean,
-  hasIconRight?: boolean,
-  hasLabelPrimary?: boolean,
-  hasLabelRight?: boolean,
-  hasLabelSecondary?: boolean,
-  iconLeft?: ReactElement,
-  iconRight?: ReactElement,
-  testID?: string,
+    labelRight?: string
+    labelPrimary?: string
+    color?: BaseInputLabelColor
 }
 
-export function BaseInputLabel(props: BaseInputLabelProps) {
-
-  const _colorGreen = props.color === 'Green';
-  const _colorGrey = props.color === 'Grey';
-  const _colorRed = props.color === 'Red';
-  const _colorWhite = props.color === 'White';
-
-  const $styles = React.useMemo(() => ({
-    root: [
-      styles.root,
-      _colorGrey && styles.rootColorGrey,
-      // _colorGreen && styles.rootColorGreen,
-      // _colorRed && styles.rootColorRed,
-      // _colorWhite && styles.rootColorWhite,
-    ],
-    label: [
-      styles.label,
-      _colorGrey && styles.labelColorGrey,
-      _colorGreen && styles.labelColorGreen,
-      _colorRed && styles.labelColorRed,
-      _colorWhite && styles.labelColorWhite,
-    ],
-    labelSecondary: [
-      styles.labelSecondary,
-      _colorGrey && styles.labelSecondaryColorGrey,
-      // _colorGreen && styles.labelSecondaryColorGreen,
-      // _colorRed && styles.labelSecondaryColorRed,
-      // _colorWhite && styles.labelSecondaryColorWhite,
-    ],
-    left: [
-      styles.left,
-      _colorGrey && styles.leftColorGrey,
-      // _colorGreen && styles.leftColorGreen,
-      // _colorRed && styles.leftColorRed,
-      // _colorWhite && styles.leftColorWhite,
-    ],
-    labelRight: [
-      styles.labelRight,
-      _colorGrey && styles.labelRightColorGrey,
-      _colorGreen && styles.labelRightColorGreen,
-      _colorRed && styles.labelRightColorRed,
-      _colorWhite && styles.labelRightColorWhite,
-    ],
-    right: [
-      styles.right,
-      _colorGrey && styles.rightColorGrey,
-      // _colorGreen && styles.rightColorGreen,
-      // _colorRed && styles.rightColorRed,
-      // _colorWhite && styles.rightColorWhite,
-    ],
-  }), [styles, props.color]);
-
-  return (
-    <View style={$styles.root} testID={props.testID}>
-      <View style={$styles.left} testID="1805:5708">
-        {props.hasIconLeft && 
-          props.iconLeft
-        }
-        {props.hasLabelPrimary && 
-          <Text style={$styles.label} testID="1805:5710">
-            {props.labelPrimary}
-          </Text>
-        }
-        {props.hasLabelSecondary && 
-          <Text style={$styles.labelSecondary} testID="1805:5711">
-            {props.labelSecondary}
-          </Text>
-        }
-      </View>
-      <View style={$styles.right} testID="1805:5712">
-        {props.hasLabelRight && 
-          <Text style={$styles.labelRight} testID="1805:5713">
-            {props.labelRight}
-          </Text>
-        }
-        {props.hasIconRight && 
-          props.iconRight
-        }
-      </View>
-    </View>
-  );
+export enum BaseInputLabelColor {
+    Dark = 'Dark',
+    Grey = 'Grey',
+    Green = 'Green',
+    Red = 'Red',
+    White = 'White',
 }
+
+const BaseInputLabel: React.FC<BaseInputLabelProps> = ({labelRight, labelPrimary, color}) => {
+    return (
+        <View style={styles.labeltype}>
+            <View style={styles.left}>
+                <Text style={styles.label}>
+                    {labelPrimary}
+                </Text>
+            </View>
+            <View style={styles.right}>
+                <Text style={styles.labelright}>
+                    {labelRight}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+export default BaseInputLabel
 
 const styles = StyleSheet.create({
-  root: {
-    flexDirection: 'row',
-    width: 312,
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-  },
-  rootColorGrey: {
-    flexDirection: 'row',
-    width: 312,
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-  },
-  iconLeft: {
-    width: 16,
-    height: 16,
-  },
-  label: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '700',
-    lineHeight: 16,
-  },
-  labelColorGrey: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '700',
-    lineHeight: 16,
-  },
-  labelColorGreen: {
-    color: theme.colors.green100,
-  },
-  labelColorRed: {
-    color: theme.colors.red100,
-  },
-  labelColorWhite: {
-    color: theme.colors.white,
-  },
-  labelSecondary: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    lineHeight: 16,
-  },
-  labelSecondaryColorGrey: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    lineHeight: 16,
-  },
-  left: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    rowGap: 4,
-    columnGap: 4,
-  },
-  leftColorGrey: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    rowGap: 4,
-    columnGap: 4,
-  },
-  labelRight: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '700',
-    lineHeight: 16,
-  },
-  labelRightColorGrey: {
-    color: theme.colors.gray70,
-    fontFamily: 'Satoshi Variable',
-    fontSize: 12,
-    fontStyle: 'normal',
-    fontWeight: '700',
-    lineHeight: 16,
-  },
-  labelRightColorGreen: {
-    color: theme.colors.green100,
-  },
-  labelRightColorRed: {
-    color: theme.colors.red100,
-  },
-  labelRightColorWhite: {
-    color: theme.colors.white,
-  },
-  iconRight: {
-    width: 16,
-    height: 16,
-  },
-  right: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    rowGap: 4,
-    columnGap: 4,
-  },
-  rightColorGrey: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    rowGap: 4,
-    columnGap: 4,
-  },
-});
+    labeltype: {
+        alignSelf: 'stretch',
+        flexShrink: 0,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'space-between',
+        columnGap: 12
+    },
+    left: {
+        flexShrink: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        columnGap: 4
+    },
+    label: {
+        flexShrink: 0,
+        textAlign: 'left',
+        color: 'rgba(76, 89, 112, 1)',
+        fontFamily: 'Satoshi Variable',
+        fontSize: 12,
+        fontWeight: '700',
+        letterSpacing: 0,
+        lineHeight: 16
+    },
+    right: {
+        flexShrink: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        columnGap: 4
+    },
+    labelright: {
+        flexShrink: 0,
+        textAlign: 'left',
+        color: 'rgba(76, 89, 112, 1)',
+        fontFamily: 'Satoshi Variable',
+        fontSize: 12,
+        fontWeight: '400',
+        letterSpacing: 0,
+        lineHeight: 16
+    }
+})
