e57ececa2ab3a6446e75db93bbcc0ad7f141a542
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-20 14:00:46 +0100

    update signUp.tsx

--- app/signUp.tsx
+++ app/signUp.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import LogoMark, { LogoSize } from '../Components/Logomark';
-import FormInputGroup from '../Components/FormInputGroup';
-import BaseLabel, { BaseInputLabelColor } from '../Components/BaseLabel';
-import ButtonRegular, { ButtonRegularSize, ButtonRegularColor } from '../Components/ButtonRegular';
-import {router} from "expo-router";
-import Divider from "../Components/Divider";
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import LogoMark, { LogoSize } from '../Components/Logomark'
+import FormInputGroup from '../Components/FormInputGroup'
+import BaseLabel, { BaseInputLabelColor } from '../Components/BaseLabel'
+import ButtonRegular, { ButtonRegularSize, ButtonRegularColor, ButtonRegularState, ButtonRegularType } from '../Components/ButtonRegular'
+import { router } from 'expo-router'
+import Divider from '../Components/Divider'
+import theme from '../Components/theme'
 
 const SignUp = () => {
-    return (
-        <View style={styles.signUp}>
+  return (
+        <View style={[styles.MainPage]}>
             <LogoMark size={LogoSize.Small} />
-            <View style={styles.buttonsocialwrapper}>
+            <View style={[styles.buttonsocialwrapper, styles.col]}>
+                <View style={styles.buttonsocial}>
                 <ButtonRegular
+                    color={ButtonRegularColor.Light}
+                    state={ButtonRegularState.Default}
                     size={ButtonRegularSize.Large}
+                    type={ButtonRegularType.Filled}
                     iconLeft="google"
                     text="Continue with Google"
                 />
+                </View>
+                <View style={styles.buttonsocial}>
                 <ButtonRegular
+                    color={ButtonRegularColor.Light}
+                    state={ButtonRegularState.Default}
                     size={ButtonRegularSize.Large}
+                    type={ButtonRegularType.Filled}
                     iconLeft="facebook"
                     text="Continue with Facebook"
                 />
+                </View>
             </View>
             <View style={styles.divider}>
-                <Divider text={"or"} type={"Horizontal"} />
+                <Divider text={'or'} type={'Horizontal'} />
             </View>
-            <View style={styles.formsignup}>
+            <View style={[styles.formsignup, styles.col]}>
                 <FormInputGroup
                     color={BaseInputLabelColor.Dark}
                     labelPrimary="Email"
@@ -44,104 +55,176 @@ const SignUp = () => {
                         <ButtonRegular
                             size={ButtonRegularSize.Large}
                             color={ButtonRegularColor.Dark}
+                            state={ButtonRegularState.Default}
+                            type={ButtonRegularType.Filled}
                             text="Create account"/>
                     </View>
                 </View>
             </View>
             <View style={styles.ctasignupwrapper}>
                 <Text style={styles.alreadyhaveanaccount}>Already have an account?</Text>
-                <TouchableOpacity onPress={()=>{router.push({pathname: "login"})}}>
+                <TouchableOpacity onPress={() => { router.push({ pathname: 'login' }) }}>
                     <Text style={styles.linksignup}>Log in</Text>
                 </TouchableOpacity>
             </View>
         </View>
-    );
-};
+  )
+}
 
-export default SignUp;
+export default SignUp
 
 const styles = StyleSheet.create({
-    signUp: {
-        flexShrink: 0,
-        height: 800,
-        width: 360,
-        backgroundColor: "rgba(255, 255, 255, 1)",
-        alignItems: "center",
-        justifyContent: "center",
-        rowGap: 24,
-        paddingVertical: 88,
-        paddingHorizontal: 0
-    },
-    buttonsocialwrapper: {
-        alignSelf: "stretch",
-        flexShrink: 0,
-        alignItems: "flex-start",
-        rowGap: 24,
-        paddingVertical: 0,
-        paddingHorizontal: 24
-    },
-    divider: {
-        alignSelf: "stretch",
-        flexShrink: 0,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        columnGap: 0,
-        paddingVertical: 0,
-        paddingHorizontal: 0
-    },
-    formsignup: {
-        alignSelf: "stretch",
-        flexShrink: 0,
-        alignItems: "flex-start",
-        rowGap: 24,
-        paddingVertical: 0,
-        paddingHorizontal: 24
-    },
-    buttonsignupwrapper: {
-        alignSelf: "stretch",
-        flexShrink: 0,
-        paddingBottom: 0,
-        alignItems: "flex-start",
-        rowGap: 0,
-        paddingHorizontal: 0
-    },
-    buttonsignup: {
-        alignSelf: "stretch",
-        flexShrink: 0,
-        backgroundColor: "rgba(9, 17, 31, 1)",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-        columnGap: 4,
-        padding: 10,
-        borderRadius: 12
-    },
-    ctasignupwrapper: {
-        flexShrink: 0,
-        flexDirection: "row",
-        alignItems: "flex-start",
-        justifyContent: "center",
-        columnGap: 4
-    },
-    alreadyhaveanaccount: {
-        flexShrink: 0,
-        textAlign: "left",
-        color: "rgba(9, 17, 31, 1)",
-        fontFamily: "Satoshi Variable",
-        fontSize: 14,
-        fontWeight: "400",
-        letterSpacing: 0,
-        lineHeight: 20
-    },
-    linksignup: {
-        flexShrink: 0,
-        textAlign: "left",
-        color: "rgba(9, 17, 31, 1)",
-        fontFamily: "Satoshi Variable",
-        fontSize: 14,
-        fontWeight: "700",
-        letterSpacing: 0,
-        lineHeight: 20
-    }
-});
+  signUp: {
+    flexShrink: 0,
+    height: 800,
+    width: 360,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    rowGap: 24,
+    paddingVertical: 88,
+    paddingHorizontal: 0
+  },
+  buttonsocialwrapper: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    alignItems: 'flex-start',
+    rowGap: 24,
+    paddingVertical: 0,
+    paddingHorizontal: 24,
+    margin: 12
+  },
+  divider: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 0,
+    paddingVertical: 0,
+    paddingHorizontal: 0
+  },
+  formsignup: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    alignItems: 'flex-start',
+    rowGap: 24,
+    paddingVertical: 0,
+    paddingHorizontal: 24
+  },
+  buttonsignupwrapper: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    paddingBottom: 0,
+    alignItems: 'flex-start',
+    rowGap: 0,
+    paddingHorizontal: 0
+  },
+  buttonsignup: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    backgroundColor: 'rgba(9, 17, 31, 1)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 4,
+    padding: 10,
+    borderRadius: 12
+  },
+  ctasignupwrapper: {
+    flexShrink: 0,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    columnGap: 4
+  },
+  alreadyhaveanaccount: {
+    flexShrink: 0,
+    textAlign: 'left',
+    color: 'rgba(9, 17, 31, 1)',
+    fontFamily: 'Satoshi Variable',
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0,
+    lineHeight: 20
+  },
+  linksignup: {
+    flexShrink: 0,
+    textAlign: 'left',
+    color: 'rgba(9, 17, 31, 1)',
+    fontFamily: 'Satoshi Variable',
+    fontSize: 14,
+    fontWeight: '700',
+    letterSpacing: 0,
+    lineHeight: 20
+  },
+  MainPage: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'flex-start',
+    overflow: 'hidden',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%'
+  },
+  col: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'flex-start',
+    overflow: 'hidden',
+    flexDirection: 'column'
+  },
+  Login: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'flex-start',
+    overflow: 'hidden',
+    flexDirection: 'row',
+    width: '80%'
+  },
+  buttonlogin: {
+    alignSelf: 'stretch',
+    flexShrink: 0,
+    backgroundColor: 'rgba(9, 17, 31, 1)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 4,
+    padding: 10,
+    borderRadius: 12
+  },
+  buttontextwrapper: {
+    flexShrink: 0,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    columnGap: 0,
+    paddingVertical: 0,
+    paddingHorizontal: 4
+  },
+  Logintext: {
+    flexShrink: 0,
+    textAlign: 'left',
+    color: 'rgba(255, 255, 255, 1)',
+    fontFamily: 'Satoshi Variable',
+    fontSize: 14,
+    fontWeight: '700',
+    letterSpacing: 0,
+    lineHeight: 20
+  },
+  buttonsocial: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: theme.colors.gray20,
+    borderWidth: 1,
+    borderRadius: 12,
+    width:'100%'
+  }
+}
+
+)
