3f448ca65fa0713d48d0f5ee1a27cf636c8d2575
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-12-26 02:20:55 +0100

    update SearchList.tsx

--- app/(aux)/search/SearchList.tsx
+++ app/(aux)/search/SearchList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Animated } from 'react-native';
 import { Svg, Path, Circle } from 'react-native-svg';
-import Icon from '../Components/Icon';
+import Icon from '../../../Components/Icon';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function SearchList() {
