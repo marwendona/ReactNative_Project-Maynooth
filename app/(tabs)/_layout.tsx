68498642fab117ff8cc7d8b9e119b4d4068344dd
Marwen Kammoun <marwenkammoun3@gmail.com>
2024-01-07 02:04:17 +0100

    update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,36 +1,116 @@
-import { StyleSheet, Text, Touchable, View } from 'react-native'
-import React from 'react'
-import { Tabs, router } from 'expo-router'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { StyleSheet, Text, Touchable, View, Image } from "react-native";
+import React from "react";
+import { Slot, Tabs, router, usePathname } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
+import NavItem from "../../Components/NavItem";
+const TabsLayout = () => {
+  const routeActive = usePathname();
 
-const HomeLayout = () => {
+  console.log(routeActive);
+  console.log("routeActive");
+  const home = routeActive === "/home" ? "Home" : "";
+  const search = routeActive === "/search" ? "Search" : "";
+  const profile = routeActive === "/profile" ? "Profile" : "";
+  console.log(home);
   return (
-    <Tabs> 
+    <Tabs>
       <Tabs.Screen
-      name='home'
-      options={{
-        headerLeft: ()=><Text>header left</Text>,
-        headerRight: ()=><TouchableOpacity onPress={()=>{router.push({pathname: "(aux)/notification"})}}><Text>header right</Text></TouchableOpacity>,
-        tabBarIcon : ({focused})=>{return (<Text>{focused ? 'oui': 'non'}</Text>)} 
-
-      }}>
-
-      </Tabs.Screen>
+        name="home"
+        options={{
+          headerLeft: () => (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              {" "}
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{ marginRight: 8 }}
+              />{" "}
+              <Image source={require("../../assets/MAYNOOTH.png")} />
+            </View>
+          ),
+          title:"",
+          headerTitle:"",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.push({ pathname: "(aux)/notification" });
+              }}
+            >
+              <Ionicons name="notifications-outline" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ focused, color, size }) => {
+            // You can return any component that you like here!
+            return <NavItem icon="home" text="Home" active={routeActive === "/home"}/>;
+          },
+        }}
+      ></Tabs.Screen>
 
       <Tabs.Screen
-      name='profile'
-      options={{
-        headerLeft: ()=><Text>header left</Text>,
-        headerRight: ()=><TouchableOpacity onPress={()=>{router.push({pathname: "(aux)/notification"})}}><Text>header right</Text></TouchableOpacity>,
-        tabBarIcon : ({focused})=>{return (<Text>{focused ? 'oui': 'non'}</Text>)} 
+        name="profile"
+        
+        options={{
+          headerLeft: () => (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              {" "}
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{ marginLeft: 8 }}
+              />{" "}
+              <Image source={require("../../assets/MAYNOOTH.png")} />
+            </View>
+          ),
+          title:"",
+          
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.push({ pathname: "(aux)/notification" });
+              }}
+            >
+              <Ionicons name="notifications-outline" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ focused, color, size }) => {
+            // You can return any component that you like here!
+            return <NavItem icon="person" text="Profile" active={routeActive === "/profile"}/>;
+          },
+        }}
+      ></Tabs.Screen>
 
-      }}>
-
-      </Tabs.Screen>
+      <Tabs.Screen
+        name="search"
+        options={{
+          headerLeft: () => (
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              {" "}
+              <Image
+                source={require("../../assets/logo.png")}
+                style={{ marginRight: 8 }}
+              />{" "}
+              <Image source={require("../../assets/MAYNOOTH.png")} />
+            </View>
+          ),
+          title:"",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.push({ pathname: "(aux)/notification" });
+              }}
+            >
+              <Ionicons name="notifications-outline" size={24} color="black" />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ focused, color, size }) => {
+            // You can return any component that you like here!
+            return <NavItem icon="search" text="Search" active={routeActive === "/search"}/>;
+          },
+        }}
+      ></Tabs.Screen>
     </Tabs>
-  )
-}
+  );
+};
 
-export default HomeLayout
+export default TabsLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
