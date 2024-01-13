545215d993bbae6e60a5af9658e08daa88c83f0a
Marwen Kammoun <marwenkammoun3@gmail.com>
2024-01-13 19:34:07 +0100

    update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,15 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
-import { Link } from 'expo-router'
+import { Link, Slot } from 'expo-router'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Index = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
 
-  const [users] = useState([{
-    name: 'test'
-  }, {
-    name: 'test2'
-  }])
-  return (
-    <View>
-      <Text>Index{count}</Text>
-
-      <Button
-        title="Increment"
-        onPress={() => dispatch({ type: 'INCREMENT' })}
-      />
-      <Button
-        title="Decrement"
-        onPress={() => dispatch({ type: 'DECREMENT' })}
-      />
-      <Link href ="/Onboarding/index1" > test </Link>
-    </View>
-  )
+    return (
+        <View>
+            <Slot></Slot>
+        </View>
+    )
 }
 
 export default Index
