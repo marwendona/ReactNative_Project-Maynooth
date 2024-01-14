497f43423a5302783244f8a034a774852ae812fb
Marwen Kammoun <marwenkammoun3@gmail.com>
2024-01-14 23:41:49 +0100

    update index.ts in store

--- store/index.ts
+++ store/index.ts
@@ -1,13 +1,15 @@
 import { combineReducers, createStore } from 'redux'
 import counterReducer from './reducers/counter'
 import productReducer from './reducers/product'
+import userReducer from "./reducers/user";
 
 const initialState = {}
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const rootReducer = combineReducers({
   counter: counterReducer,
-  product: productReducer
+  product: productReducer,
+  user: userReducer
 })
 
 // const store = createStore(counterReducer)
